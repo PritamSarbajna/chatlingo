da53256e38a5d6c509e7f07b8952a23839fc738f
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2023-08-13 09:59:37 +0530

    changed

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -145,8 +145,8 @@ const Register = () => {
           <div className="flex justify-center self-center z-10">
             <div className="p-12 bg-white mx-auto rounded-2xl w-100 z-10">
               <div className="mb-4">
-                <h3 className="font-semibold text-2xl text-gray-800">Sign In </h3>
-                <p className="text-gray-500">Please sign in to your account.</p>
+                <h3 className="font-semibold text-2xl text-gray-800">Sign Up </h3>
+                <p className="text-gray-500">Please register your account.</p>
               </div>
               {
                 error && 
